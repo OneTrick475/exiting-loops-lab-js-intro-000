bea0fb40534dbcc5c0bc37ddc79aef88886973f7
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-04 07:39:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function breakOut(array, changeValue, stopValue){
   for(var i=0; i<array.length; i++){
-    if(array[i] !== stopValue){
-      array[i] = changeValue;
-    }
-    else {
-     break;
-    }
+    if(array[i] === stopValue){
+      break;
+    }else {array[i] = changeValue;}
+
     return array;
   }
   }
