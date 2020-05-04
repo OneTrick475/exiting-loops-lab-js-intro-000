7b840fa91c3c79003c1ba86fbb683fa1fa186c41
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-04 07:44:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function breakOut(array, changeValue, stopValue){
     return array;
 
   }
+
+function keepGoing(array, changeValue, skipValue){
+  for(var i=0; i<array.length; i++){
+    if(array[i] === skipValue){continue;}
+    else{array[i] = changeValue;}
+  }
+  return array
+}
